8701030275b7e0753eccebf64edb5a4f3567077d
kadjialan <alankadji7@gmail.com>
2023-02-04 15:05:26 +0100

    complete design

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,58 +1,107 @@
 import React from 'react';
 import './Main.css';
-import logo2 from '../Images/logo2.png';
+import logo2 from '../Images/logo3.png';
 
 function Main() {
   return (
-    <div>
-      <div className="home main">
-        <div className="main__holder__left">
-          <div className="main__wallet">
-            <i className="fa-solid fa-wallet" />
-            <h3>Wallet</h3>
-          </div>
+    <div className="main">
+      <div className="main__holder__left">
+        <div className="main__wallet">
+          <i className="fa-solid fa-wallet" />
+          <h3>Wallet</h3>
+        </div>
 
-          <div className="main__heading">
-            <p>
-              <b>Default Currenncy:</b>
-            </p>
-            <select id="currencies">
+        <div className="main__heading">
+          <p>
+            <b>Default Currenncy:</b>
+          </p>
+          <select id="currencies">
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="XAF">XAF</option>
+            <option value="XAF">CNY</option>
+          </select>
+        </div>
+
+        <div className="currency__options">
+          <input type="number" value="USD" />
+          <span>USD</span>
+        </div>
+
+        <div className="currency__options">
+          <input type="number" value="EUR" />
+          <span>EUR</span>
+        </div>
+
+        <div className="currency__options">
+          <input type="number" value="XAF" />
+          <span>XAF</span>
+        </div>
+
+        <div className="currency__options">
+          <input type="number" value="CNY" />
+          <span>CNY</span>
+        </div>
+
+        <button className="save" type="button">
+          save
+        </button>
+        <div>
+          <h3>Total currency in XAF</h3>
+          <div className="total__currency">waiting</div>
+        </div>
+      </div>
+
+      <div className="main__holder__right">
+        <img src={logo2} alt="logo" className="logo2" />
+        <h1>Currency Converter</h1>
+        <h3>
+          <i>Below are some of the activities you can perform</i>
+        </h3>
+        <div className="main__holder__deposit">
+          <h3 className="right__headings">Deposite</h3>
+          <div>
+            <span>Amount :</span>
+            <input type="number" />
+          </div>
+          <div>
+            <span>Currency for Deposite:</span>
+            <select id="currencies2">
               <option value="USD">USD</option>
               <option value="EUR">EUR</option>
               <option value="XAF">XAF</option>
               <option value="XAF">CNY</option>
             </select>
           </div>
-
-          <div className="currency__options">
-            <span>USD</span>
-            <input type="number" value="USD" />
-          </div>
-
-          <div className="currency__options">
-            <span>EUR</span>
-            <input type="number" value="EUR" />
-          </div>
-
-          <div className="currency__options">
-            <span>XAF</span>
-            <input type="number" value="XAF" />
-          </div>
-
-          <div className="currency__options">
-            <span>CNY</span>
-            <input type="number" value="CNY" />
-          </div>
-
-          <div>
-            <h3>Total currency in XAF</h3>
-            <div className="total__currency">waiting</div>
-          </div>
+          <button type="button" className="confirm">
+            confirm
+          </button>
         </div>
 
-        <div className="main__holder__right">
-          <h1>Currency Converter</h1>
-          <img src={logo2} alt="logo" className="logo2" />
+        <div className="main__holder__deposit">
+          <h3 className="right__headings">Exhange currencies</h3>
+          <div>
+            <span>Amount :</span>
+            <input type="number" />
+          </div>
+          <div className="exchanger">
+            <select id="currencies2">
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="XAF">XAF</option>
+              <option value="XAF">CNY</option>
+            </select>
+            <i className="fa-sharp fa-solid fa-arrow-right" />
+            <select id="currencies2">
+              <option value="XAF">XAF</option>
+              <option value="XAF">CNY</option>
+              <option value="EUR">EUR</option>
+              <option value="USD">USD</option>
+            </select>
+          </div>
+          <button type="button" className="confirm">
+            confirm
+          </button>
         </div>
       </div>
     </div>
